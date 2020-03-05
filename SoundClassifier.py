8bd172f14c08b86e41fc138c18ab588955a8587a
Tak Hogan <takhogan@gmail.com>
2020-03-05 14:26:38 -0600

    added spectrum graphing

--- SoundClassifier.py
+++ SoundClassifier.py
@@ -8,11 +8,33 @@ import numpy as np
 import scipy.io.wavfile as wavfile
 import plotly
 import plotly.graph_objs as go
-layout = go.Layout(title='Spectrogram Calculation Example',
-                   xaxis=dict(title='time (sec)',),
-                   yaxis=dict(title='Freqs (Hz)',))
+from pydub import AudioSegment
+import os
+import pandas as pd
+import plotly.express as px
+import glob
+
+'''
+https://gist.github.com/arjunsharma97/0ecac61da2937ec52baf61af1aa1b759
+'''
+def m4_to_wav(filename):
+    (path, file_extension) = os.path.splitext(filename)
+    file_extension_final = file_extension.replace('.', '')
+    try:
+        track = AudioSegment.from_file(filename,
+                                       file_extension_final)
+        wav_filepath = filename.replace(file_extension_final, 'wav')
+        print('CONVERTING: ' + str(wav_filepath))
+        file_handle = track.export(wav_filepath, format='wav')
+        # os.remove(filepath)
+    except Exception:
+        print('Error with ' + filename)
+    return wav_filepath
 
 
+'''
+https://github.com/tyiannak/multimodalAnalysis/blob/master/audio/example02.py
+'''
 def get_fft_spec(signal, fs, win):
     frame_size, signal_len, spec, times = int(win * fs), len(signal), [], []
     # break signal into non-overlapping short-term windows (frames)
@@ -23,12 +45,54 @@ def get_fft_spec(signal, fs, win):
         # append mag of fft
         X = np.abs(scp.fft(f)) ** 2
         freqs = np.arange(0, 1, 1.0/len(X)) * (fs/2)
-        spec.append(X[0:int(len(X)/2)] / X.max())
+        spec.append(X[0:int(len(X) / 2)] / X.max())
+    freqs = freqs[:int(freqs.shape[0] / 2)]
     return np.array(spec).T, freqs, times
 
+
+def graph_fft(filename, reload=True):
+    layout = go.Layout(title='Spectrogram ' + filename,
+                       xaxis=dict(title='time (sec)', ),
+                       yaxis=dict(title='Freqs (Hz)', ))
+
+    (pre_extention, extention) = os.path.splitext(filename)
+    spectrum_filename = pre_extention + '_S.npy'
+    frequency_filename = pre_extention + '_f.npy'
+    time_filename = pre_extention + '_t.npy'
+
+    if reload:
+        [Fs, s] = wavfile.read(filename)
+        S, f, t = get_fft_spec(s, Fs, 0.02)
+        S = np.nan_to_num(S)
+
+        np.save(spectrum_filename, S)
+        np.save(frequency_filename, f)
+        np.save(time_filename, t)
+    else:
+        S = np.load(spectrum_filename)
+        f = np.load(frequency_filename)
+        t = np.load(time_filename)
+
+    # print(len(t))
+    magnitude_means = np.median(S, axis=1)
+    magnitude_means = magnitude_means / np.linalg.norm(magnitude_means)
+    df_data = np.array([f, magnitude_means]).T
+    # print(df_data.shape)
+    # exit(0)
+    freq_hist = pd.DataFrame(data=df_data, columns=['freqs', 'means'])
+    fig = go.Figure([go.Bar(x=freq_hist['freqs'],
+                            y=freq_hist['means'])],
+                    layout=dict(title=filename))
+    fig.update_yaxes(range=[0, 1])
+    fig.show()
+    # exit(0)
+    # heatmap = go.Heatmap(z=S, y=f, x=t)
+    # plotly.offline.plot(go.Figure(data=[heatmap], layout=layout),
+    #                     filename="temp.html", auto_open=True)
+
 if __name__ == '__main__':
-    [Fs, s] = wavfile.read("sample_music.wav")
-    S, f, t = get_fft_spec(s, Fs, 0.02)
-    heatmap = go.Heatmap(z=S, y=f, x=t)
-    plotly.offline.plot(go.Figure(data=[heatmap], layout=layout),
-                        filename="temp.html", auto_open=True)
+    filenames = glob.glob('audio/*.m4a')
+    for filename in filenames:
+        wav_filename = m4_to_wav(filename)
+        graph_fft(wav_filename)
+
---
+++ venv/lib/python3.7/site-packages/sksound/tests/test_sound.py
@@ -0,0 +1,69 @@
+import sys
+import os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(myPath, '..', '..'))
+
+from sksound import sounds 
+import unittest
+import numpy as np
+
+class TestSequenceFunctions(unittest.TestCase):
+    
+    def test_sound_generate(self):
+        # Test with self-generated data
+        rate = 22050
+        dt = 1./rate
+        t = np.arange(0,0.5,dt)
+        freq = 880
+        x = np.sin(2*np.pi*freq*t)
+        sounddata = np.int16(x*2**13)
+        inSound = sounds.Sound(inData=sounddata, inRate=rate)
+        inSound.play()
+        
+        # Test if type conversion works
+        inSound2 = sounds.Sound(inData=x, inRate=rate)
+        inSound2.play()
+        
+    def test_sound_read(self):
+        # Single channel, stereo, and mp3 input
+        sound_files = ['peas.wav', 'tiger.wav', 'YouAreNotIt.mp3']
+        for file in sound_files:
+            try:
+                inSound = sounds.Sound(file)
+                inSound.play()
+            except sounds.NoFFMPEG_Error:
+                pass
+            
+        # Also make sure that
+        # a) also another input gets read in correctly, and
+        # b) that floats are converted properly to integers
+        inSound.read_sound('float_sound.wav')
+        inSound.play()
+        
+    def test_sound_select(self):
+        # Test with GUI
+        inSound = sounds.Sound()
+        inSound.play()
+        print('hi')
+        
+        # Info about sound, and its display
+        (source, rate, numChannels, totalSamples, duration, bitsPerSample) = inSound.get_info()
+        inSound.summary()
+        print('ho')
+        
+    def test_sound_write(self):
+        # Test with self-generated data
+        rate = 22050
+        dt = 1./rate
+        t = np.arange(0,0.5,dt)
+        freq = 880
+        x = np.sin(2*np.pi*freq*t)
+        sounddata = np.int16(x*2**13)
+        inSound = sounds.Sound(inData=sounddata, inRate=rate)
+        
+        # Write sound-data
+        inSound.write_wav()
+        
+if __name__ == '__main__':
+    unittest.main()
+    
